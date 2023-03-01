2a8cf9f7df037ca62050c97ef8e33919358aedaf
René van Erp <reneverp@gmail.com>
2023-03-01 13:56:29 +0100

    Initial commit

---
+++ src/BasicCmakeSetup.cpp
@@ -0,0 +1,12 @@
+﻿// BasicCmakeSetup.cpp : Defines the entry point for the application.
+//
+
+#include "BasicCmakeSetup.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
